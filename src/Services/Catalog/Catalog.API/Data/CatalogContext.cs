974e620ebf50f322d0e4bb7728b4a1d775070ffb
ThirenG187 <30898510+ThirenG187@users.noreply.github.com>
2022-03-06 18:29:54 +0200

    * Repository implemented and controllers for Catalog have been completed.

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,22 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data;
+
+public class CatalogContext : ICatalogContext
+{    
+    public CatalogContext(IConfiguration configuration)
+    {
+        string connectionString = configuration.GetValue<string>("DatabaseSettings:ConnectionString");
+        string dbName = configuration.GetValue<string>("DatabaseSettings:DatabaseName");
+        string collectionName = configuration.GetValue<string>("DatabaseSettings:CollectionName");
+
+        var client = new MongoClient(connectionString);
+        var database = client.GetDatabase(dbName);
+
+        Products = database.GetCollection<Product>(collectionName);
+        CatalogContextSeed.SeedData(Products);
+    }
+
+    public IMongoCollection<Product> Products { get; }
+}
