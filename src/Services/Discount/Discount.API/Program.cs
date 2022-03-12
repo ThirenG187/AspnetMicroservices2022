9d487895772f7988c64e5bb2f6f2356f3616bfd7
ThirenG187 <30898510+ThirenG187@users.noreply.github.com>
2022-03-12 07:04:18 +0200

    * Discount.API added * Discount.API has been containerized.

--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,6 +1,10 @@
+using Discount.API.Repositories;
+using Npgsql;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -8,6 +12,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.MigrateDatabase<Program>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -21,3 +27,60 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+public static class HostExtensions
+{
+    public static IHost MigrateDatabase<T>(this IHost host, int? retry = 0)
+    {
+        int retryForAvailablility = retry!.Value;
+
+        using var scope = host.Services.CreateScope();
+        var services = scope.ServiceProvider;
+        var configuration = services.GetRequiredService<IConfiguration>();
+        var logger = services.GetRequiredService<ILogger<T>>();
+
+        try
+        {
+            logger.LogInformation("Migrating postresql database.");
+            using var connection = new NpgsqlConnection(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            connection.Open();
+
+            using var command = new NpgsqlCommand
+            {
+                Connection = connection
+            };
+
+            command.CommandText = "DROP TABLE IF EXISTS Coupon";
+            command.ExecuteNonQuery();
+
+            command.CommandText = @"CREATE TABLE Coupon(Id SERIAL PRIMARY KEY,
+                                                        ProductName VARCHAR(24) NOT NULL,
+                                                        Description TEXT,
+                                                        Amount INT)";
+            command.ExecuteNonQuery();
+
+            command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('IPhone X', 'IPhone Discount', 150);";
+            command.ExecuteNonQuery();
+
+            command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('Samsung 10', 'Samsung Discount', 100);";
+            command.ExecuteNonQuery();
+
+            logger.LogInformation("Migrated postresql database.");
+
+            connection.Close();
+        }
+        catch (NpgsqlException ex)
+        {
+            logger.LogError(ex, "An error occured while migrating the postgresql database");
+
+            if(retry < 10)
+            {
+                retryForAvailablility++;
+                Thread.Sleep(2000);
+                MigrateDatabase<T>(host, retryForAvailablility);
+            }
+        }
+
+        return host;
+    }
+}
